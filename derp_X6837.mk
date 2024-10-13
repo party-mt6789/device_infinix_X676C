fe14761243adc818c7bb3d29eb31ff1dbbe4348a
fjrXTR <fajarslebew31@gmail.com>
2024-10-13 04:17:29 +0000

    X6837: Derp-ify!
    Change-Id: I816383b07282288e00c82a912f42a767537a02ac
    Signed-off-by: fjrXTR <fajarslebew31@gmail.com>

--- derp_X6837.mk
+++ derp_X6837.mk
@@ -9,13 +9,16 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+$(call inherit-product, vendor/derp/config/common_full_phone.mk)
+TARGET_BOOT_ANIMATION_RES := 1080
+TARGET_USES_MINI_GAPPS := true
+PRODUCT_NO_CAMERA := false
 
 # Inherit from X6837 device
 $(call inherit-product, device/infinix/X6837/device.mk)
 
 BOARD_VENDOR := Infinix
-PRODUCT_NAME := lineage_X6837
+PRODUCT_NAME := derp_X6837
 PRODUCT_DEVICE := X6837
 PRODUCT_MANUFACTURER := INFINIX
 PRODUCT_BRAND := Infinix
@@ -28,6 +31,3 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME=X6837-OP
 
 BUILD_FINGERPRINT := Infinix/X6837-OP/Infinix-X6837:13/TP1A.220624.014/240313V1154:user/release-keys
-
-# Time
-LINEAGE_VERSION_APPEND_TIME_OF_DAY := true
