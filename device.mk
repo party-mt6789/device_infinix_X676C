a864edc3179f1aa02142c01090bc72a052acfb6b
Woomymy <woomy@woomy.be>
2024-11-12 00:44:02 +0800

    X6837: Enable USB audio accessory support
    Change-Id: Ib5c2fee5a774ebd1153d46f856f1208fe2227bbe
    Signed-off-by: Woomymy <woomy@woomy.be>
    Signed-off-by: Shirayuki39 <lorddemecrius83@proton.me>
    Signed-off-by: fjrXTR <fajarslebew31@gmail.com>

--- device.mk
+++ device.mk
@@ -454,6 +454,8 @@ PRODUCT_PACKAGES += \
     vendor.lineage.touch@1.0-service.transsion
 
 # USB
+$(call soong_config_set,android_hardware_mediatek_usb,audio_accessory_supported,true)
+
 PRODUCT_PACKAGES += \
     android.hardware.usb-service.mediatek \
     android.hardware.usb.gadget-service.mediatek
