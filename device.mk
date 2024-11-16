f4957a5e105cf5f8dea9caaa9ede5e61604f7440
fjrXTR <fajarslebew31@gmail.com>
2024-11-16 16:14:22 +0000

    X676C: Switch to Transsion vibrator services
    Signed-off-by: fjrXTR <fajarslebew31@gmail.com>

--- device.mk
+++ device.mk
@@ -462,7 +462,7 @@ PRODUCT_PACKAGES += \
     
 # Vibrator
 PRODUCT_PACKAGES += \
-    android.hardware.vibrator-service.mediatek
+    android.hardware.vibrator-service.transsion
 
 # VNDK
 PRODUCT_COPY_FILES += \
